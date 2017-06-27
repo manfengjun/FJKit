289d2325ce86a3d7281fd64f64388a0843f47602
jun <2546863989@qq.com>
2017-06-27 16:59:42 +0800

    JUN 添加依赖

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform:ios, '8.0'
+use_frameworks!
+target 'FJKit' do
+    pod 'Alamofire', '~> 4.4.0'
+end
---
+++ Pods/Target Support Files/Pods-FJKit/Pods-FJKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FJKit : NSObject
+@end
+@implementation PodsDummy_Pods_FJKit
+@end
